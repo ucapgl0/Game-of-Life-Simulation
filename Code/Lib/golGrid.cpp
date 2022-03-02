34bfb03fbe311933cb4001e8bc4c0312cbe5954d
Leo.Liang <ucapgl0@ucl.ac.uk>
2022-03-02 13:58:29 +0000

    add function random, input file, alive neighbour

--- Code/Lib/golGrid.cpp
+++ Code/Lib/golGrid.cpp
@@ -48,4 +48,92 @@ namespace gol {
 		cout << endl;
 	}
 
+    grid::grid(int rows, int cols, int alives) :rows(rows), cols(cols) {
+		//grid::grid(rows, cols, alive) {};
+		srand(time(0));
+		vector<string> random_v;
+		for (int i = 0; i < alives; i++) random_v.push_back("o");
+		for (int i = alives; i < rows*cols; i++)random_v.push_back("-");
+		random_shuffle(random_v.begin(), random_v.end());
+
+		position_data = random_v;
+	}
+
+
+	grid::grid(string file) {
+		ifstream ifs;
+		ifs.open(file, ios::in);
+		if (!ifs.is_open()) {
+			cout << "file open unsuccessfully" << endl;
+		}
+
+		char buf[1024] = { 0 };
+		while (ifs.getline(buf, sizeof(buf))) {
+			cout << buf << endl;
+		}
+	}
+
+
+	int grid::alives_neighbour(int row, int col) {
+		int count = 0;
+		if (row > rows || col > cols) {
+
+		}
+		else if (row < 1 || col < 1) {
+
+		}
+		else {
+			// left-up
+			if (row - 2 > -1 && col - 2 > -1) {
+				if (position_data[(row - 2)*cols+ col-2] == "o") {
+					count = count + 1;
+				}
+			}
+			// left-down
+			if (row < rows && col - 2 > -1) {
+				if (position_data[row*cols + col - 2] == "o") {
+					count = count + 1;
+				}
+			}
+			//left
+			if (col - 2 > -1) {
+				if (position_data[(row - 1)*cols + col - 2] == "o") {
+						count = count + 1;
+				}
+			}
+			//up
+			if (row - 2 > -1) {
+				if (position_data[(row - 2)*cols + col - 1] == "o") {
+					count = count + 1;
+				}
+			}
+			//down
+			if (row < rows) {
+				if (position_data[row*cols + col - 1] == "o") {
+					count = count + 1;
+				}
+			}
+			//right
+			if (col < cols) {
+				if (position_data[(row - 1)*cols + col] == "o") {
+					count = count + 1;
+				}
+			}
+			//right-up
+			if (row - 2 > -1 && col < cols) {
+				if (position_data[(row - 2)*cols + col] == "o") {
+					count = count + 1;
+				}
+			}
+			//right-down
+			if (row < rows && col < cols) {
+				if (position_data[row*cols + col] == "o") {
+					count = count + 1;
+				}
+			}
+			
+		}
+		return count;
+	}
+
 }
